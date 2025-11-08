b64f9112ed1ff6ed4319a51ab41738170122826e
Snag-hub <imsnag.1@gmail.com>
2025-11-08 10:25:15 +0530

    Revert treesitter configuration to fix 'gd' command

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,102 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
   config = function()
     require("nvim-treesitter.configs").setup({
-      -- === YOUR CORE SETUP (required) ===
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "tsx",
-        "json",
-        "bash",
-        "markdown",
-        "cpp",
-      },
-      sync_install = true,
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "html", "css" },
+      sync_install = false,
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
-
-      -- === MY FULL TEXTOBJECTS CONFIG (your power) ===
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Around local scope" },
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@function.outer"] = "V",
-            ["@class.outer"] = "<c-v>",
-          },
-          include_surrounding_whitespace = true,
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-            ["]a"] = "@parameter.outer",
-            ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-            ["]o"] = "@loop.*",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-            ["]A"] = "@parameter.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-            ["[a"] = "@parameter.outer",
-            ["[s"] = { query = "@local.scope", query_group = "locals", desc = "Previous scope" },
-            ["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold" },
-            ["[o"] = "@loop.*",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-            ["[A"] = "@parameter.outer",
-          },
-          goto_next = { ["]d"] = "@conditional.outer" },
-          goto_previous = { ["[d"] = "@conditional.outer" },
-        },
-        swap = {
-          enable = true,
-          swap_next = { ["<leader>a"] = "@parameter.inner" },
-          swap_previous = { ["<leader>A"] = "@parameter.inner" },
-        },
-      },
     })
-
-    -- === REPEATABLE MOVES (unforgettable) ===
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { desc = "Repeat move next" })
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { desc = "Repeat move previous" })
-    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
   end,
 }
