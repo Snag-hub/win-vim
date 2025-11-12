a6b42bd6facd77856003deaef81cd16e672523d1
Snag-hub <imsnag.1@gmail.com>
2025-11-12 12:55:15 +0530

    fix: Correct trouble.nvim keymap placement in community.lua

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -44,4 +44,34 @@ return {
     --         require("colorizer").setup()
     --     end,
     -- },
+    --
+    -- Git integration
+    -- {
+    --     "NeogitOrg/neogit",
+    --     dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+    --     config = true,
+    -- },
+    --
+    -- Enhanced diagnostics
+    -- {
+    --     "folke/trouble.nvim",
+    --     dependencies = { "nvim-tree/nvim-web-devicons" },
+    --     config = function()
+    --         require("trouble").setup({})
+    --         vim.keymap.set("n", "<leader>t", function() require("trouble").toggle() end, { desc = "Toggle Trouble" })
+    --         vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = "Workspace Diagnostics" })
+    --         vim.keymap.set("n", "<leader>d", function() require("trouble").toggle("document_diagnostics") end, { desc = "Document Diagnostics" })
+    --         vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("quickfix") end, { desc = "Quickfix List" })
+    --         vim.keymap.set("n", "<leader>l", function() require("trouble").toggle("loclist") end, { desc = "Location List" })
+    --         vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = "LSP References" })
+    --     end,
+    -- },
+    --
+    -- Distraction-free writing
+    -- {
+    --     "folke/zen-mode.nvim",
+    --     config = function()
+    --         require("zen-mode").setup({})
+    --     end,
+    -- },
 }
